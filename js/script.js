57484fdbb0e33ecbbbe3242fa6348fe507d7e738
Andrew Sorokin <a.50r0kin@yandex.ru>
2021-08-05 18:53:05 +0300

    Отображение модального окна после полной прокрутки сайта

--- js/script.js
+++ js/script.js
@@ -100,7 +100,7 @@ document.addEventListener('DOMContentLoaded', () => {
   function openModal() {
     modal.classList.toggle('show');
     document.body.style.overflow = 'hidden';
-    clearInterval(modalTimerId)
+    clearInterval(modalTimerId);
   }
 
   function closeModal() {
@@ -127,4 +127,13 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   const modalTimerId = setTimeout(openModal, 5000);
+
+  function showModalByScroll() {
+    if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+      openModal();
+      window.removeEventListener('scroll', showModalByScroll);
+    }
+  }
+
+  window.addEventListener('scroll', showModalByScroll);
 });
