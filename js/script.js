a5c65f5ae389fc8641fc94de27d04715b12ccdbd
Andrew Sorokin <a.50r0kin@yandex.ru>
2021-08-09 13:58:25 +0300

    Отправка данных с помощью fetch

--- js/script.js
+++ js/script.js
@@ -232,10 +232,6 @@ document.addEventListener('DOMContentLoaded', () => {
       `;
       form.insertAdjacentElement('afterend', statusMessage);
 
-      const request = new XMLHttpRequest();
-      request.open('POST', 'server.php');
-
-      request.setRequestHeader('Content-type', 'application/json');
       const formData = new FormData(form);
 
       const object = {};
@@ -243,20 +239,21 @@ document.addEventListener('DOMContentLoaded', () => {
         object[key] = value;
       });
 
-      const json = JSON.stringify(object);
-
-      request.send(json);
-
-      request.addEventListener('load', () => {
-        if (request.status === 200) {
-          console.log(request.response);
-          showThanksModal(message.success);
-          form.reset();
-          statusMessage.remove();
-        } else {
-          showThanksModal(message.failure);
-        }
-      });
+      fetch('server.php', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(object)
+      }).then(data => data.text())
+      .then(data => {
+        console.log(data);
+        showThanksModal(message.success);
+        
+        statusMessage.remove();
+      }).catch(() => {
+        showThanksModal(message.failure);
+      }).finally(() => form.reset());
     });
   }
 
@@ -283,4 +280,14 @@ document.addEventListener('DOMContentLoaded', () => {
       closeModal();
     }, 4000);
   }
+
+  fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    body: JSON.stringify({name: 'Alex'}),
+    headers: {
+      'Content-type': 'application/json'
+    }
+  })
+    .then(response => response.json())
+    .then(json => console.log(json));
 });
